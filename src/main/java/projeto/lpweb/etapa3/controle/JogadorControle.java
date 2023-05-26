e18406caf542e6442b5047854f42a14bb2347388
daniel99korban <d.lima@acad.ifma.edu.br>
2023-05-25 23:19:00 -0300

    adicionadas rotas com filtro de busca para jogador

--- src/main/java/projeto/lpweb/etapa3/controle/JogadorControle.java
+++ src/main/java/projeto/lpweb/etapa3/controle/JogadorControle.java
@@ -1,7 +1,10 @@
 
 package projeto.lpweb.etapa3.controle;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import projeto.lpweb.etapa3.modelo.Jogador;
 import org.springframework.web.bind.annotation.*;
+import projeto.lpweb.etapa3.repositorio.JogadorRepositorio;
 
 /**
  *
@@ -10,19 +13,48 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class JogadorControle {
     
-    @GetMapping("/")
-    public String teste(){
-        return "Olá Mundo!";
+    @Autowired
+    private JogadorRepositorio acao;// ações de crud
+    
+    @PostMapping("/cadastrar-jogador")
+    public Jogador cadastro(@RequestBody Jogador j){
+        return acao.save(j);
     }
     
-    @GetMapping("teste/{nome}")
-    public String teste(@PathVariable String nome){
-        return "Olá " + "<b>" + nome.toUpperCase() + "</b>";
+    @GetMapping("/listar-jogadores")
+    public List<Jogador> listarJogadores(){
+        return acao.findAll();
     }
     
-    @PostMapping("/jogador")
-    public Jogador jogador(@RequestBody Jogador j){
-        return j;
-    }
+    @GetMapping("selecionar-nome/{nome}")
+    public List<Jogador> selecionarPorNome(@PathVariable String nome){
+        return acao.findByNome(nome);
+    } 
     
+    @GetMapping("selecionar-codigo/{codigo}")
+    public Jogador selecionarPorCodigo(@PathVariable int codigo){
+        return acao.findById(codigo);
+    }    
 }
+
+
+
+
+
+
+
+//
+//    @GetMapping("/")
+//    public String teste(){
+//        return "Olá Mundo!";
+//    }
+//    
+//    @GetMapping("teste/{nome}")
+//    public String teste(@PathVariable String nome){
+//        return "Olá " + "<b>" + nome.toUpperCase() + "</b>";
+//    }
+//    
+//    @PostMapping("/jogador")
+//    public Jogador jogador(@RequestBody Jogador j){
+//        return j;
+//    }
