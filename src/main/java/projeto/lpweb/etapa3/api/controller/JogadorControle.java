cac4cdca4d8afdea9dcbbd65bb4bdb35f76ef8b3
DanielKorban <74277609+daniel99korban@users.noreply.github.com>
2023-06-02 22:28:44 +0000

    sugestão de retorno de função

--- src/main/java/projeto/lpweb/etapa3/api/controller/JogadorControle.java
+++ src/main/java/projeto/lpweb/etapa3/api/controller/JogadorControle.java
@@ -25,6 +25,7 @@ public class JogadorControle {
     }
     
     @GetMapping
+    // ResponseEntity<Jogador>
     public List<Jogador> listarJogadores(){
         return service.listarTodos();
     }
